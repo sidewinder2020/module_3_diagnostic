b13b85652f2bebb01bafd9eab8f955cb7f8a334a
sidewinder2020 <sdwinder1@gmail.com>
2017-10-18 09:58:59 -0600

    Begin AltFuelServiceStation file, still trying to find starting api url

---
+++ services/alt_fuel_station_service.rb
@@ -0,0 +1,14 @@
+class AltFuelStationService
+  def initialize
+    @conn = Faraday.new('https://api.data.gov/api/alt-fuel-stations/v1') do |faraday|
+      faraday.headers["X-API-KEY"] = "cFSRX90qhxdoTDp0pmUuuM2aWewha8RLmoHusJfG"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def response
+    response = @conn.get("")
+    JSON.parse(response.body)
+  end
+
+end
