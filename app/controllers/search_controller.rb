615d01d43e5d1bffaeeef0452511e8279beef394
sidewinder2020 <sdwinder1@gmail.com>
2017-10-18 11:09:08 -0600

    no implicit conversion of String into Integer ERROR - has to do with zipcode

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @stations = StationSearch.new(params['q']).station
+    @stations = StationSearch.new(params['q'].to_i).stations
   end
 end
