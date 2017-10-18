042e553a02351777887386e67d4288707dd680da
sidewinder2020 <sdwinder1@gmail.com>
2017-10-18 10:21:03 -0600

    main url working for service - now to work on endpoint

--- app/services/alt_fuel_station_service.rb
+++ app/services/alt_fuel_station_service.rb
@@ -1,14 +1,14 @@
 class AltFuelStationService
 
   def initialize
-    @conn = Faraday.new('https://api.data.gov/api/alt-fuel-stations/v1') do |faraday|
+    @conn = Faraday.new('https://developer.nrel.gov/api/alt-fuel-stations/v1') do |faraday|
       faraday.headers["X-API-KEY"] = "cFSRX90qhxdoTDp0pmUuuM2aWewha8RLmoHusJfG"
       faraday.adapter Faraday.default_adapter
     end
   end
 
   def response(zipcode)
-    response = @conn.get("#{zipcode}")
+    response = @conn.get("/nearest/#{zipcode}.json")
     JSON.parse(response.body)
   end
 
